31e0518f851bccb71f6326590ed3f026b2adfa43
Micah-Powell <MicahPowell2024@gmail.com>
2026-02-19 14:05:42 -0500

    first push

--- findlines.c
+++ findlines.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-char line[1000];
+
 
 void search_file(FILE * file, char * search_query) {
   /**
   Searches `file` for lines containing `search_query` and prints matching lines
   */
-  
+  char *line = malloc(100);
+
   while (fgets(line, sizeof(line), file)) {
+    while (strchr(line, "\n") == NULL){
+      char *line = malloc(sizeof(*line * 2)); 
+      fgets(line, sizeof(line), file);
+    }
     if (strstr(line, search_query)) {
       printf("%s", line);
     }
   }
+
+  free(line);
 }
 
 int main(int argc, char ** argv) {
