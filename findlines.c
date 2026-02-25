0b7fff80a21e2e9f653b51895700d6251b05bc0a
Micah-Powell <MicahPowell2024@gmail.com>
2026-02-24 23:12:49 -0500

    I think this works

--- findlines.c
+++ findlines.c
@@ -9,12 +9,30 @@ void search_file(FILE * file, char * search_query) {
   Searches `file` for lines containing `search_query` and prints matching lines
   */
   char *line = malloc(100);
+  if (!line) {
+    exit(1);
+  }
   int linesize = 100;
   while (fgets(line, linesize, file)) {
-    while (strchr(line, '\n') == NULL){
-      line = malloc(linesize * 2); 
+    
+    size_t len = strlen(line);
+
+    while (len > 0 && line[len - 1] != '\n'){
+      char *temp = realloc(line, linesize * 2); 
+      if (!temp){
+        free(line);
+        exit(2);
+      }
+      line = temp;
       linesize = linesize * 2;
-      fgets(line, linesize, file);
+      
+      
+      if (!fgets(line + len, linesize - len, file)){
+        
+        break;
+      }
+      len = strlen(line);
+      
     }
     if (strstr(line, search_query)) {
       printf("%s", line);
@@ -27,7 +45,7 @@ void search_file(FILE * file, char * search_query) {
 int main(int argc, char ** argv) {
   if (argc != 2) {
     printf("Exactly one search parameter required\n");
-    exit(1);
+    exit(3);
   }
 
   search_file(stdin, argv[1]);
